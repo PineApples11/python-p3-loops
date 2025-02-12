faed9e6baf7fe4bd4c984ccc019ad0d9c775b71f
PineApples11 <kinyanjuiivy1@gmail.com>
2025-02-12 17:29:37 +0300

    looping in conditions

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
 
+    for i in range(10,0,-1):
+    #   if i == 1 :
+    #     print ("Happy New Year!")
+    #   else:
+        print(i) 
+    print("Happy New Year!")
+    # return  0
+ 
+    # code goes here!
+#     pass
+# print(happy_new_year()) 
 def square_integers(int_list):
     # code goes here!
-    pass
+    set_list=[]
+    for i in int_list:
+        # if i > 0:
+        set_list.append(i*i)
+        # else:
+        #     print("number is less than o")
+    return set_list
+
+# print(square_integers([1,2,3]))
+    # pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for n in range(1,101):
+        if n % 15 == 0:
+            print("FizzBuzz")
+        elif n % 5 == 0:
+            print("Buzz")
+        elif n % 3 == 0:
+            print("Fizz")
+        else:
+            print(n)
+    return ("invalid")
+        
+# print(fizzbuzz(45))
